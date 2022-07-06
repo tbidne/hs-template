a2c637b882f8bf9fcc3e8fdac0e23ceb7a04b895
Tommy Bidne <tbidne@protonmail.com>
2022-07-07 00:05:07 +1200

    Improve ghc_base nixpkgs hash retrieval

--- nix/ghc_base.nix
+++ nix/ghc_base.nix
@@ -1,11 +1,25 @@
 { compilerVersion
-, hash ? (builtins.fromJSON (builtins.readFile ../flake.lock)).nodes.nixpkgs.locked.rev
+, hash ? null
 }:
 
 let
+  # If a hash is not provided, read it from the flake.lock's top-level
+  # nixpkgs.
+  hash' =
+    if hash == null
+    then
+      let
+        lockJson = builtins.fromJSON (builtins.readFile ../flake.lock);
+        # The hash referred to by "nixpkgs" in flake.lock does not necessarily
+        # correspond to "nixpkgs" in flake.nix. We have to retrieve it via
+        # 'root'.
+        nixpkgsKey = lockJson.nodes.root.inputs.nixpkgs;
+      in
+      lockJson.nodes.${nixpkgsKey}.locked.rev
+    else hash;
   pkgs = import
     (fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/${hash}.tar.gz";
+      url = "https://github.com/NixOS/nixpkgs/archive/${hash'}.tar.gz";
     })
     { };
   compiler = pkgs.haskell.packages."${compilerVersion}";

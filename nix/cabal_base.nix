b9894ccbdd6345bafe020bf23604a8481de11de8
Tommy Bidne <tbidne@protonmail.com>
2022-07-29 11:24:44 +1200

    Simplify nix

--- nix/cabal_base.nix
+++ nix/cabal_base.nix
@@ -3,25 +3,7 @@
 }:
 
 let
-  # If a hash is not provided, read it from the flake.lock's top-level
-  # nixpkgs.
-  hash' =
-    if hash == null
-    then
-      let
-        lockJson = builtins.fromJSON (builtins.readFile ../flake.lock);
-        # The hash referred to by "nixpkgs" in flake.lock does not necessarily
-        # correspond to "nixpkgs" in flake.nix. We have to retrieve it via
-        # 'root'.
-        nixpkgsKey = lockJson.nodes.root.inputs.nixpkgs;
-      in
-      lockJson.nodes.${nixpkgsKey}.locked.rev
-    else hash;
-  pkgs = import
-    (fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/${hash'}.tar.gz";
-    })
-    { };
+  pkgs = (import ./lib.nix).get-pkgs hash;
   compiler = pkgs.haskell.packages."${compilerVersion}";
 in
 pkgs.mkShell {

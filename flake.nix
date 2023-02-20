e74ad60fe32ed4309b9fe988b09f849873344fdd
Tommy Bidne <tbidne@protonmail.com>
2023-02-20 16:52:05 +1300

    Update nix to 9.4.4, swap flake-utils for flake-parts

--- flake.nix
+++ flake.nix
@@ -4,45 +4,48 @@
     url = "github:edolstra/flake-compat";
     flake = false;
   };
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   outputs =
-    { flake-compat
-    , flake-utils
+    inputs@{ flake-compat
+    , flake-parts
     , nixpkgs
     , self
     }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      buildTools = c: with c; [
-        cabal-install
-        pkgs.gnumake
-        pkgs.zlib
-      ];
-      # add tools like hlint, ormolu, ghcid here if you want them
-      # on the PATH
-      devTools = c: with c; [
-        ghcid
-        haskell-language-server
-      ];
-      ghc-version = "ghc924";
-      compiler = pkgs.haskell.packages."${ghc-version}";
-      mkPkg = returnShellEnv: withDevTools:
-        compiler.developPackage {
-          inherit returnShellEnv;
-          name = "hs-template";
-          root = ./.;
-          modifier = drv:
-            pkgs.haskell.lib.addBuildTools drv
-              (buildTools compiler ++
-                (if withDevTools then devTools compiler else [ ]));
-        };
-    in
-    {
-      packages.default = mkPkg false false;
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      perSystem = { pkgs, ... }:
+        let
+          buildTools = c: with c; [
+            cabal-install
+            pkgs.gnumake
+            pkgs.zlib
+          ];
+          devTools = c: with c; [
+            # do not run ghcid tests, as they require stack
+            (pkgs.haskell.lib.dontCheck ghcid)
+            haskell-language-server
+          ];
+          ghc-version = "ghc944";
+          compiler = pkgs.haskell.packages."${ghc-version}";
+          mkPkg = returnShellEnv: withDevTools:
+            compiler.developPackage {
+              inherit returnShellEnv;
+              name = "hs-template";
+              root = ./.;
+              modifier = drv:
+                pkgs.haskell.lib.addBuildTools drv
+                  (buildTools compiler ++
+                    (if withDevTools then devTools compiler else [ ]));
+            };
+        in
+        {
+          packages.default = mkPkg false false;
 
-      devShells.default = mkPkg true true;
-      devShells.ci = mkPkg true false;
-    });
+          devShells.default = mkPkg true true;
+          devShells.ci = mkPkg true false;
+        };
+      systems = [
+        "x86_64-linux"
+      ];
+    };
 }
